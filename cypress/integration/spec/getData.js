626476368d8d8ef43156a9aeeb47cffb56140611
AdinaFakih <addy.fakih@gmail.com>
2022-02-23 23:06:36 +0500

    trying to add conditional checks

--- cypress/integration/spec/getData.js
+++ cypress/integration/spec/getData.js
@@ -6,59 +6,81 @@ describe('Get data from Parachains.info Page', () => {
     // })
 
     // it('Verify Project list is present', () => {
+    //     cy.get('[id="parachains_table"]').should('be.exist');   //get table tag
     //     cy.get('th[class="project_column sorting"]').scrollIntoView().should('be.visible');
     //     cy.wait(1000);
     // })
 
-    it('Verify menu Links are  present', () => {
-        //cy.get('[id="parachains_table"]').should('be.exist');   //get table tag
-        const visited = new Set();
-        const toVisit = ['/'];  //https://parachains.info
+    it('Crawler', () => {
+        cy.visit('/');
+        cy.get('img[class="parachain_logo_img"]').should('be.visible');
+        //cy.get('#parachains_table').children().debug();
+        cy.get('[id="parachains_table"]').should('be.exist');   //get table tag
+        cy.get('th[class="project_column sorting"]').scrollIntoView().should('be.visible');
+        cy.get('#parachains_table>tbody>tr>td>div>div>a').each(($el) => {
+            const herf = $el.attr('href');
+            cy.log(herf);
         
-        //cy.get('th[class="project_column sorting"]').scrollIntoView().should('be.visible');
-        function visitNextUrl(){
-            if (toVisit.length === 0) {
-                return
-            }
-            //cy.log(`to visit: ${toVisit.join(', ')}`);
-            const url = toVisit.shift(); //breadth first search 
-            visited.add(url);
-            cy.visit(url)
-            cy.get('img[class="parachain_logo_img"]').should('be.visible');
-            cy.wait(1000);
-            //cy.get('[id="parachains_table"]').children();  
-        // for (let index = 0; index < 161; index++) {
-            cy.get('table>tbody>tr>td>div>div>a')
-                .should(Cypress._.noop)
-                .then(($links)=> {
-                    const links = $links
-                        .toArray()
-                        .map((link)=> link.getAttribute('href'))
-                        .filter((url) => !url.startsWith('http') && !url.startsWith('//'))
-                        .filter((url) => !visited.has(url))
-                        .filter((url) => !toVisit.includes(url))
-                    console.log(links)
-                    toVisit.push(...links)
-                //expect($links).to.contain('parachain_url')
-                    cy.wait(2000);
-            }).then(visitNextUrl)
-                .should('contain', 'https://parachains.info')
-                .should('have.class', 'active')
-                .and('have.attr', 'href');
-            //.and('include', 'cypress.io');
-        }
-        visitNextUrl();
-            //cy.get('table>tbody>tr').click();
-            //cy.get('[data-id="'+index+'"] > .project_column > .dd-flex > .media > .media-body > .d-flex > .mr-2 > .font-weight-bold').click();
-            //cy.wait(2000);
-            //cy.go(-1) // equivalent to clicking back button
-       // }
-        //cy.get('table>tbody>tr>td>div>div>a')
-        //cy.get('[id="parachains_table"]').get('td').each(elem => {});
-        //cy.get("a[title='Live for Teams']").first().should('be.exist');
+            // then I will do your test:
+            cy.visit(herf);
+            cy.title().should('include', 'Parachains.info');
+            cy.get('section').then((section) => {
+                if (body.find('[class="fab fa-twitter fa-lg"]').length < 0) {  //section has tag i less than 
+                    cy.log("no twitter");
+                }
+                else {
+                    cy.get('[class="fab fa-twitter fa-lg"]').should('not.be.visible')
+                }
+            //cy.get('[class="fab fa-twitter fa-lg"]').should('not.be.visible')
+            })
+        
+        });
+        
+        // cy.get('#parachains_table>tbody>tr>td>div>div>a')
+        //     .invoke('attr', 'href')
+        //     .then(href => {
+        //         console.log(href);
+        //         cy.visit(href);
+        //.children().debug();
 
-        //*[@id="parachains_table"]/tbody/tr[@data-id="763"]
-
-        //table>tbody>tr>td>div>div>a
+        //.each(elem => {});
+        //.get('tr').get('td')
+        //});
     })
+
+
+    // it('Iterate through list of all projects', () => {
+    //     const visited = new Set();
+    //     const toVisit = ['/'];  //https://parachains.info
+        
+    //     function visitNextUrl(){
+    //         if (toVisit.length === 0) {
+    //             return
+    //         }
+    //         //cy.log(`to visit: ${toVisit.join(', ')}`);
+    //         const url = toVisit.shift(); //breadth first search 
+    //         visited.add(url);
+    //         cy.visit(url);
+    //         cy.get('img[class="parachain_logo_img"]').should('be.visible');
+    //         cy.wait(1000);
+    //         cy.get('table>tbody>tr>td>div>div>a')
+    //             .should(Cypress._.noop)
+    //             .then(($links)=> {
+    //                 const links = $links
+    //                     .toArray()
+    //                     .map((link)=> link.getAttribute('href'))
+    //                     .filter((url) => !url.startsWith('http') && !url.startsWith('//'))
+    //                     .filter((url) => !visited.has(url))
+    //                     .filter((url) => !toVisit.includes(url))
+    //                 console.log(links)
+    //                 toVisit.push(...links)
+    //                 cy.wait(2000);
+    //                 // if(cy.get('[class="fab fa-twitter fa-lg"]').should('not.be.visible')){
+    //                 //     return
+    //                 // }
+    //             })
+    //             cy.get('[class="fab fa-twitter fa-lg"]').then(visitNextUrl);
+    //     }
+    //     visitNextUrl();
+    // })
 })
